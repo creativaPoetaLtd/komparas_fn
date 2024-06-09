8c120150df0e47a9cc40dea7f753b8b9675f3e1e
FabCode67 <mwanafunzifabrice@gmail.com>
2024-06-09 09:57:17 +0200

    mobile search

--- src/components/home/HomeMobileNav.tsx
+++ src/components/home/HomeMobileNav.tsx
@@ -1,12 +1,49 @@
 import { Menu, Dropdown, Button } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { HiBars3BottomLeft } from "react-icons/hi2";
 import { FaSearch } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
+import { getAllProducts } from '../../api/product';
 
 
 
 const MobileHomeNav = () => {
+  // const location = useLocation();
+  // const [selectedMenu, setSelectedMenu] = useState('home');
+  const [searchValue, setSearchValue] = useState("");
+  const [, setSearchedId] = useState<string>("");
+  const [autocompleteOptions, setAutocompleteOptions] = useState<{ id: string, name: string }[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await getAllProducts();
+      const allProducts = response?.data?.products;
+
+      const productNames = allProducts?.map((product: any) => ({
+        id: product._id,
+        name: product.product_name,
+      }));
+
+      setAutocompleteOptions(productNames);
+    };
+
+    fetchProducts();
+  }, []);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleOptionSelect = (event: any) => {
+    const selectedOption:any = autocompleteOptions.find(option => option.name === event.target.value);
+    if (selectedOption) {
+      setSearchedId(selectedOption.id);
+      navigate(`/product/${selectedOption.id}`);
+      console.log('Selected Product ID:', selectedOption.id);
+    }
+  };
+  const navigate = useNavigate();
   const menu = (
     <Menu>
       <Menu.Item key="0">
@@ -95,10 +132,24 @@ const MobileHomeNav = () => {
         </div>
       </nav>
       <div className='searchBar flex justify-center md:w-3/5 bg-white w-[96%] rounded-md self-center mt-2 mb-2 items-center border-gray-300 border'>
-        <input type="text" placeholder="Search" className="t text-black w-full h-full p-2 outline-none rounded-md " />
+        <input type="text" value={searchValue}
+          onChange={handleInputChange}
+          onInput={handleOptionSelect}
+          list="autocomplete-options"
+          placeholder="Search" className="t text-black w-full h-full p-2 outline-none rounded-md " />
+           <datalist id="autocomplete-options">
+          {autocompleteOptions.map((option, index) => (
+            <option key={index} value={option.name} />
+          ))}
+        </datalist>
         <button className="rounded-md">
           <FaSearch className="text-lg text-black" />
         </button>
+        <Dropdown overlay={menu} className='mt-1'>
+        <Button>
+          <UserOutlined className='test text-white' />
+        </Button>
+      </Dropdown>
       </div>
     </div>
   );
