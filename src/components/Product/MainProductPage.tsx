16428ee866e00cb2967f0076c1583f31a54e09a0
FabCode67 <mwanafunzifabrice@gmail.com>
2024-06-28 22:12:38 +0200

    fixing

--- src/components/Product/MainProductPage.tsx
+++ src/components/Product/MainProductPage.tsx
@@ -1,151 +1,137 @@
 import React, { useState } from 'react';
 import { CiCircleChevLeft, CiCircleChevRight } from 'react-icons/ci';
+import { useSwipeable } from 'react-swipeable';
 
 interface Product {
-    products: any
+  products: any;
 }
 
 const MainProductPage: React.FC<Product> = ({ products }) => {
-    const [selectedImageIndex, setSelectedImageIndex] = useState(0);
-    const [touchStartX, setTouchStartX] = useState(0);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
-    const handleImageClick = (index: number) => {
-        setSelectedImageIndex(index);
-    };
+  const handleImageClick = (index: number) => {
+    setSelectedImageIndex(index);
+  };
 
-    const handleNextImage = () => {
-        setSelectedImageIndex((prevIndex) => (prevIndex + 1) % products?.product?.product_images.length);
-    };
+  const handleNextImage = () => {
+    setSelectedImageIndex((prevIndex) => (prevIndex + 1) % products?.product?.product_images.length);
+  };
 
-    const handlePrevImage = () => {
-        setSelectedImageIndex((prevIndex) => (prevIndex - 1 + products?.product?.product_images.length) % products?.product?.product_images.length);
-    };
+  const handlePrevImage = () => {
+    setSelectedImageIndex((prevIndex) => (prevIndex - 1 + products?.product?.product_images.length) % products?.product?.product_images.length);
+  };
 
-    const handleTouchStart = (e: React.TouchEvent) => {
-        setTouchStartX(e.touches[0].clientX);
-    };
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: handleNextImage,
+    onSwipedRight: handlePrevImage,
+  });
 
-    const handleTouchMove = (e: React.TouchEvent) => {
-        const touchEndX = e.touches[0].clientX;
-        if (touchStartX - touchEndX > 50) {
-            handleNextImage();
-        } else if (touchStartX - touchEndX < -50) {
-            handlePrevImage();
-        }
-    };
-
-    return (
-        <div className='w-full pl-0 flex flex-col h-fit'>
-            <div className="w-full h-fit flex lg:flex-row flex-col ">
-                <div className="flex md:flex-row flex-col lg:w-[50%] w-full m-auto justify-center items-center h-full">
-                    <div className="md:flex hidden md:w-[20%] md:h-[450px] md:space-y-4 space-y-0 py-4 md:overflow-y-auto w-full md:flex-col justify-between">
-                        {products?.product?.product_images?.map((image: any, index: number) => (
-                            <div key={index} className={`otherImages md:w-full w-[55px] items-start flex justify-start md:h-[138px] h-[50px] ${selectedImageIndex === index ? 'border-2 border-blue-500' : ''}`} onClick={() => handleImageClick(index)}>
-                                <img src={image.product_image} width={100} height={100} alt="" className={` ${selectedImageIndex === index ? 'border-2 border-yellow-600 rounded-md p-2' : ''} md:w-full w-[55px] md:h-[89px] h-[49px] object-contain`} />
-                            </div>
-                        ))}
-                    </div>
-                    <div className="md:w-[80%] w-full h-full">
-                        <div className='w-full mt-12'>
-                            <h1 className="text-xl w-[20rem] mt-12 lg:pl-32 md:pl-16 pl-2 font-semibold">{products?.product?.product_name}</h1>
-                        </div>
-                        <div className="MainIMageDiv md:w-[500px] w-[350px] md:h-[600px] h-[285px] flex m-auto justify-center items-center relative"
-                            onTouchStart={handleTouchStart}
-                            onTouchMove={handleTouchMove}>
-                            <button onClick={handlePrevImage} className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-green-50 text-white px-2 py-1 rounded-md">
-                                <CiCircleChevLeft className="bg-green-300 text-2xl text-yellow-500 rounded-full hover:bg-green-800" />
-                            </button>
-                            <img src={products?.product?.product_images[selectedImageIndex]?.product_image} alt="" className="md:w-[446px] w-full md:h-[315px] h-full object-contain" />
-                            <button onClick={handleNextImage} className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-green-50 text-white px-2 py-1 rounded-md">
-                                <CiCircleChevRight className="bg-green-300 text-2xl text-yellow-500 rounded-full hover:bg-green-800" />
-                            </button>
-                        </div>
-                    </div>
-                    <div className='md:hidden w-full overflow-x-auto'>
-                        <div className="md:hidden flex h-fit py-4 overflow-x-auto w-fit justify-between flex-row">
-                            {products?.product?.product_images?.map((image: any, index: number) => (
-                                <div key={index} className={`otherImages md:w-[170px] w-[95px] items-start flex justify-start md:h-[100px] h-[90px] ${selectedImageIndex === index ? 'border-2 border-blue-500' : ''}`} onClick={() => handleImageClick(index)}>
-                                    <img src={image.product_image} width={150} height={100} alt="" className={` ${selectedImageIndex === index ? 'border-2 border-green-600 rounded-md w-[70px] md:h-[89px] h-[75px]' : 'border-gray-600 border rounded-md'} md:w-[100px] p-[1px] w-[70px] md:h-[89px] h-[75px] object-contain`} />
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-                <div className="flex lg:flex-col md:flex-row sm:flex-col flex-col space-y-4 mx-auto items-start justify-start md:py-20 py-4 lg:w-[40%] w-full ">
-                    <div className="md:w-[415px] w-full p-2 flex flex-col space-y-3">
-                        <div className='flex space-x-3'>
-                            {products?.product?.availableStorages?.map((storage: any, index: number) => (
-                                <div key={index} className="flex items-center justify-center text-sm w-fit px-2 py-1 h-fit border-2 border-black">
-                                    <p className="font-bold">{storage?.value}</p>
-                                </div>
-                            ))}
-                        </div>
-                        <div className='flex space-x-3'>
-                            <h1 className="text-base text-red-500 font-light line-through">
-                                {products?.product?.vendor_prices?.reduce((prev: any, current: any) => (prev.price < current.price) ? prev : current).price
-                                    .toLocaleString('en-US', { maximumFractionDigits: 4 })} Rwf
-                            </h1>
-                            <h1 className='realprice font-semibold'>
-                                {products?.product?.our_price.toLocaleString('en-US', { maximumFractionDigits: 4 })} Rwf
-                            </h1>
-                        </div>
-                        <p className="text-sm text-justify">{products?.product?.product_description}</p>
-                    </div>
-                    <div className="line md:w-[412px] w-full h-[1px] md:hidden flex lg:flex bg-[#EDB62E]"></div>
-                    <div className="flex flex-col text-sm shopTable">
-                        <table className="w-full">
-                            <thead>
-                                <p className="text-green-500 px-2 item-start m-auto text-start">Aho wayisanga</p>
-                            </thead>
-                            <tbody>
-                                {products?.product?.vendors?.map((shop: any, index: number) => (
-                                    <tr key={index + 12}>
-                                        <td className="text-[#353535] item-start m-auto p-2">{shop?.name}</td>
-                                        {products?.product?.vendor_prices?.map((price: any, priceIndex: number) => (
-                                            price?.vendor_id === shop?._id && (
-                                                <td key={priceIndex} className="text-[#353535] item-start m-auto p-2">
-                                                    {price?.price}
-                                                </td>
-                                            )
-                                        ))}
-                                        {products?.product?.vendor_prices?.map((price: any, priceIndex: number) => (
-                                            price?.vendor_id === shop?._id && (
-                                                <>
-                                                    {price?.colors.length >= 1 && (
-                                                        <td key={priceIndex} className="text-[#353535] flex item-start m-auto p-2">
-                                                            {JSON.parse(JSON.stringify(price?.colors).replace(/[\"\#]+/g, '').replace(/(\w+)/g, '"$1"')).map((color: any, colorIndex: number) => (
-                                                                <div key={colorIndex} style={{
-                                                                    backgroundColor: `${color ? color : '#0a0a0a'}`,
-                                                                }} className={` bg-[#${color ? color : '#0a0a0a'}] rounded-full h-4 flex w-4 m-1`}></div>
-                                                            ))}
-                                                        </td>
-                                                    )}
-                                                    {price?.colors?.length <= 0 && (
-                                                        <td className="text-[#353535] flex item-start m-auto p-2">
-                                                            {price?.colors.map((color: any, colorIndex: number) => (
-                                                                <div key={colorIndex} style={{
-                                                                    backgroundColor: `${color}`,
-                                                                }} className={` bg-[#${color}] rounded-full h-4 flex w-4 m-1`}></div>
-                                                            ))}
-                                                        </td>
-                                                    )}
-                                                </>
-                                            )
-                                        ))}
-                                        {products?.product?.availableStorages?.map((storage: any, storageIndex: number) => (
-                                            <td key={storageIndex} className="text-[#353535] item-start m-auto p-2">
-                                                {storage.value ? storage.value : 'N/A'}
-                                            </td>
-                                        ))}
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+  return (
+    <div className='w-full pl-0 flex flex-col h-fit'>
+      <div className="w-full h-fit flex lg:flex-row flex-col ">
+        <div className="flex md:flex-row flex-col lg:w-[50%] w-full m-auto justify-center items-center h-full">
+          <div className="md:flex hidden md:[w-20%] md:h-[450px] md:space-y-4 space-y-0 py-4 md:overflow-y-auto w-full md:flex-col justify-between">
+            {products?.product?.product_images?.map((image: any, index: number) => (
+              <div key={index} className={`otherImages md:w-full w-[55px] items-start flex justify-start md:h-[138px] h-[50px] ${selectedImageIndex === index ? 'border-2 border-blue-500' : ''}`} onClick={() => handleImageClick(index)}>
+                <img src={image.product_image} width={100} height={100} alt="" className={` ${selectedImageIndex === index ? 'border-2 border-yellow-600 rounded-md p-2' : ''} md:w-full w-[55px] md:h-[89px] h-[49px] object-contain`} />
+              </div>
+            ))}
+          </div>
+          <div className="md:w-[80%] w-full h-full">
+            <div className='w-full mt-12'>
+              <h1 className="text-xl w-[20rem] mt-12 lg:pl-32 md:pl-16 pl-2 font-semibold">{products?.product?.product_name}</h1>
             </div>
+            <div {...swipeHandlers} className="MainIMageDiv md:w-[500px] w-[350px] md:h-[600px] h-[285px] flex m-auto justify-center items-center relative">
+              <button onClick={handlePrevImage} className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-green-50 text-white px-2 py-1 rounded-md">
+                <CiCircleChevLeft className="bg-green-300 text-2xl text-yellow-500 rounded-full hover:bg-green-800" />
+              </button>
+              <img src={products?.product?.product_images[selectedImageIndex]?.product_image} alt="" className="md:w-[446px] w-full md:h-[315px] h-full object-contain" />
+              <button onClick={handleNextImage} className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-green-50 text-white px-2 py-1 rounded-md">
+                <CiCircleChevRight className="bg-green-300 text-2xl text-yellow-500 rounded-full hover:bg-green-800" />
+              </button>
+            </div>
+          </div>
+          <div className='md:hidden w-full overflow-x-auto'>
+            <div className="md:hidden flex h-fit py-4 overflow-x-auto w-fit justify-between flex-row">
+              {products?.product?.product_images?.map((image: any, index: number) => (
+                <div key={index} className={`otherImages md:w-[170px] w-[95px] items-start flex justify-start md:h-[100px] h-[90px] ${selectedImageIndex === index ? 'border-2 border-blue-500' : ''}`} onClick={() => handleImageClick(index)}>
+                  <img src={image.product_image} width={150} height={100} alt="" className={` ${selectedImageIndex === index ? 'border-2 border-green-600 rounded-md w-[70px] md:h-[89px] h-[75px]' : 'border-gray-600 border rounded-md'} md:w-[100px] p-[1px] w-[70px] md:h-[89px] h-[75px] object-contain`} />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-    );
+        <div className="flex lg:flex-col md:flex-row sm:flex-col flex-col space-y-4 mx-auto items-start justify-start md:py-20 py-4 lg:w-[40%] w-full">
+          <div className="md:w-[415px] w-full p-2 flex flex-col space-y-3">
+            <div className='flex space-x-3'>
+              {products?.product?.availableStorages?.map((storage: any, index: number) => (
+                <div key={index} className="flex items-center justify-center text-sm w-fit px-2 py-1 h-fit border-2 border-black">
+                  <p className="f font-bold">{storage?.value}</p>
+                </div>
+              ))}
+            </div>
+            <div className='flex space-x-3'>
+              <h1 className="text-base text-red-500 font-light line-through">
+                {products?.product?.vendor_prices?.reduce((prev: any, current: any) => (prev.price < current.price) ? prev : current).price.toLocaleString('en-US', { maximumFractionDigits: 4 })} Rwf
+              </h1>
+              <h1 className='realprice font-semibold'>
+                {products?.product?.our_price.toLocaleString('en-US', { maximumFractionDigits: 4 })} Rwf
+              </h1>
+            </div>
+            <p className="text-sm text-justify">{products?.product?.product_description}</p>
+          </div>
+          <div className="line md:w-[412px] w-full h-[1px] md:hidden flex lg:flex bg-[#EDB62E]"></div>
+          <div className="flex flex-col text-sm shopTable">
+            <table className="w-full">
+              <thead>
+                <p className="text-green-500 px-2 item-start m-auto text-start">Aho wayisanga</p>
+              </thead>
+              <tbody>
+                {products?.product?.vendors?.map((shop: any, index: number) => (
+                  <tr key={index + 12}>
+                    <td className="text-[#353535] item-start m-auto p-2">{shop?.name}</td>
+                    {products?.product?.vendor_prices?.map((price: any, priceIndex: number) => (
+                      price?.vendor_id === shop?._id && (
+                        <td key={priceIndex} className="text-[#353535] item-start m-auto p-2">
+                          {price?.price}
+                        </td>
+                      )
+                    ))}
+                    {products?.product?.vendor_prices?.map((price: any, priceIndex: number) => (
+                      price?.vendor_id === shop?._id && (
+                        <>
+                          {price?.colors.length >= 1 && (
+                            <td key={priceIndex} className="text-[#353535] flex item-start m-auto p-2">
+                              {JSON.parse(JSON.stringify(price?.colors).replace(/[\"\#]+/g, '').replace(/(\w+)/g, '"$1"')).map((color: any, colorIndex: number) => (
+                                <div key={colorIndex} style={{
+                                  backgroundColor: `${color ? color : '#0a0a0a'}`,
+                                }} className={`bg-[#${color ? color : '#0a0a0a'}] rounded-full h-4 flex w-4 m-1`}></div>
+                              ))}
+                            </td>
+                          )}
+                          {price?.colors?.length <= 0 && (
+                            <td key={priceIndex} className="text-[#353535] flex item-start m-auto p-2">
+                              {[...Array(4).keys()].map((_, index) => (
+                                <div key={index} style={{
+                                  backgroundColor: `#${
+                                    Math.random().toString(16).slice(2, 8).padEnd(6, '0').slice(0, 6).toUpperCase()
+                                  }`
+                                }} className={`bg-[#0a0a0a] rounded-full h-4 flex w-4 m-1`}></div>
+                              ))}
+                            </td>
+                          )}
+                        </>
+                      )
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainProductPage;
