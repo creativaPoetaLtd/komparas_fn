8e60d825e2df77a4d86c7b4845d22bb1be9932f3
FabCode67 <mwanafunzifabrice@gmail.com>
2024-07-17 19:03:00 +0200

    adding loading skeleton on finance report while data are pending

--- src/components/Product/Shop/Map.tsx
+++ src/components/Product/Shop/Map.tsx
@@ -11,7 +11,7 @@ const GoogleMapSection: React.FC<GoogleMapSectionProps> = ({ apiKey }) => {
       <iframe
         src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=Space+Needle,Seattle+WA`}
         width="100%"
-        height="450"
+        height="1"
         style={{ border: 0 }}
         allowFullScreen={true}
         loading="lazy"
