3d442cae5fbc0133f0b3b3a6e2757ad34841b1bf
FabCode67 <mwanafunzifabrice@gmail.com>
2024-07-17 18:58:20 +0200

    adding loading skeleton on finance report while data are pending

--- src/components/Product/Shop/Map.tsx
+++ src/components/Product/Shop/Map.tsx
@@ -1,35 +1,21 @@
-import React, { useState } from "react";
-import * as GoogleMapsApi from '@react-google-maps/api';
-
-const { GoogleMap, LoadScript } = GoogleMapsApi;
+import React from "react";
 interface GoogleMapSectionProps {
   apiKey: string;
 }
 
 const GoogleMapSection: React.FC<GoogleMapSectionProps> = ({ apiKey }) => {
-  const [, setMap] = useState<google.maps.Map | null>(null);
 
-  const onLoad = (mapInstance: google.maps.Map) => {
-    setMap(mapInstance);
-  };
 
   return (
     <div className="GoogleMapSection md:block hidden mt-12">
-      <LoadScript googleMapsApiKey={apiKey} id="google-map-script">
-        <GoogleMap
-          id="shop-map"
-          mapContainerStyle={{
-            height: "400px",
-            width: "100%",
-          }}
-          zoom={10}
-          center={{
-            lat: -2.001373,
-            lng: 30.061884,
-          }}
-          onLoad={onLoad}
-        />
-      </LoadScript>
+      <iframe
+        src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=Space+Needle,Seattle+WA`}
+        width="100%"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen={true}
+        loading="lazy"
+      ></iframe>
     </div>
   );
 };
