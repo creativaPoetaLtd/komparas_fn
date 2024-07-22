f344e376da78e18bede76889f73b86459e8623c2
FabCode67 <mwanafunzifabrice@gmail.com>
2024-07-22 13:57:25 +0200

    fixing

--- src/components/dashboard/components/shop/EditShop.tsx
+++ src/components/dashboard/components/shop/EditShop.tsx
@@ -113,8 +113,8 @@ const EditShopForm = ({ setIsEditShop }: EditShopFormProps) => {
 
     return (
         <div className="w-full h-full bg-black bg-opacity-50 top-0 left-0 flex justify-center items-center">
-            <div className="w-[60%] h-fit bg-gray-300 pb-10 my-12 rounded-md flex flex-col justify-center items-center">
-                <div className="w-full flex justify-between items-center px-4 py-2">
+            <div className="w-full px-12 my-12 h-fit bg-gray-300 pb-10 rounded-md flex flex-col justify-center items-center">
+            <div className="w-full flex justify-between items-center px-4 py-2">
                     <h1 className="text-slate-900 text-lg font-bold pl-48">Edit Shop</h1>
                     <button className="w-10 h-10 rounded-md bg-slate-200 hover:bg-red-600 flex justify-center items-center" onClick={handleShopFormClose}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-slate-900 hover:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -123,7 +123,7 @@ const EditShopForm = ({ setIsEditShop }: EditShopFormProps) => {
                     </button>
                 </div>
                 <div className="w-full flex justify-center items-center">
-                    <form className="w-full flex flex-col justify-center items-center md:px-20 px-2 bg-gray-300" onSubmit={handleEditShop}>
+                    <form className="w-full flex flex-col justify-center items-center md:px-52 px-2 bg-gray-300" onSubmit={handleEditShop}>
                         <div className="w-full flex flex-col justify-start items-start bg-white px-10 py-5 rounded-md">
                             <div className="w-full flex flex-col justify-start items-start">
                                 <label className="w-full flex justify-start items-start text-slate-900">
