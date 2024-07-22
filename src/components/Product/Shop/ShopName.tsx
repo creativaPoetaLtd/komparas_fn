28b0c72f00badc0ceb648e468d99a6c2fe613860
FabCode67 <mwanafunzifabrice@gmail.com>
2024-07-22 13:40:01 +0200

    fixing

--- src/components/Product/Shop/ShopName.tsx
+++ src/components/Product/Shop/ShopName.tsx
@@ -6,7 +6,13 @@ import { MdEmail, MdOutlineEmail } from "react-icons/md";
 
 const ShopName = ({ shopData }: any) => {
     const [showSharePanel, setShowSharePanel] = useState(false);
+    const [showAdressPanel, setShowAdressPanel] = useState(false);
+    const [showKomparasCodePanel, setShowKomparasCodePanel] = useState(false);
 
+    const toggleKomparasCodePanel = () => {
+        setShowKomparasCodePanel(!showKomparasCodePanel);
+    };
+    const codeOfShop = Math.floor(Math.random() * 1000000);
     const toggleSharePanel = () => {
         setShowSharePanel(!showSharePanel);
     };
@@ -15,6 +21,11 @@ const ShopName = ({ shopData }: any) => {
         setShowSharePanel(false);
     };
 
+    const toggleAdressPanel = () => {
+        setShowAdressPanel(!showAdressPanel);
+    }
+
+    
     return (
         <>
             <div className="flex w-full space-x-12 md:px-4 px-1">
@@ -32,27 +43,27 @@ const ShopName = ({ shopData }: any) => {
             </div>
             <div className="flex md:space-x-6 space-x-2 mt-12 md:px-8 px-1">
                 <div className="flex flex-col">
-                    <div className="text-sm text-[#353535] items-center flex m-auto h-10 w-10 rounded-full bg-yellow-600 mx-auto justify-center">
-                        <a href={`tel:${shopData?.phone}`} className="flex justify-center m-auto">
+                    <a href={`tel:${shopData?.phone}`} className="text-sm text-[#353535] items-center flex m-auto h-10 w-10 rounded-full bg-yellow-600 mx-auto justify-center">
+                        <p  className="flex justify-center m-auto">
                             <IoCallOutline className="text-white text-2xl" />
-                        </a>
-                    </div>
+                        </p>
+                    </a>
                     <p className="md:text-sm text-xs  text-[#353535]">Duhamagare</p>
                 </div>
                 <div className="flex flex-col">
-                    <div className="text-sm text-[#353535] items-center flex m-auto h-10 w-10 rounded-full bg-yellow-600 mx-auto justify-center">
-                        <a href={`mailto:${shopData?.email}`} className="flex justify-center m-auto">
+                    <button onClick={toggleAdressPanel} className="text-sm text-[#353535] items-center flex m-auto h-10 w-10 rounded-full bg-yellow-600 mx-auto justify-center">
+                        <p className="flex justify-center m-auto">
                             <MdOutlineEmail className="text-white text-2xl" />
-                        </a>
-                    </div>
+                        </p>
+                    </button>
                     <p className="md:text-sm text-xs  text-[#353535]">Tugereho</p>
                 </div>
                 <div className="flex flex-col">
-                    <div className="text-sm text-[#353535] items-center flex m-auto h-10 w-10 rounded-full bg-yellow-600 mx-auto justify-center">
-                        <a href={shopData?.phone} className="flex justify-center items-center m-auto">
+                    <button onClick={toggleKomparasCodePanel} className="text-sm text-[#353535] items-center flex m-auto h-10 w-10 rounded-full bg-yellow-600 mx-auto justify-center">
+                        <p className="flex justify-center items-center m-auto">
                             <img src="/dd.png" alt="whatsapp" className="w-full h-full rounded-full self-center m-auto" />
-                        </a>
-                    </div>
+                        </p>
+                    </button>
                     <p className="md:text-sm text-xs  text-[#353535]">Komparas Code</p>
                 </div>
                 <div className="flex flex-col">
@@ -86,6 +97,42 @@ const ShopName = ({ shopData }: any) => {
                                 <MdEmail className="text-white text-xl flex my-auto justify-center" />
                                 <span className="text-white flex my-auto justify-center mt-[1.9px]">Email</span>
                             </a>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {showAdressPanel && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                    <div className="bg-white rounded-lg shadow-lg p-4 w-80">
+                        <div className="flex justify-between items-center mb-4">
+                            <h2 className="md:text-lg font-semibold">Tugereho:</h2>
+                            <button className="text-gray-600 hover:text-gray-900" onClick={toggleAdressPanel}>&times;</button>
+                        </div>
+                        <div className="flex flex-col space-y-2">
+                            <p className="text-[#353535] text-sm">
+                                <b>Duherereye</b> : {shopData?.location}, {' '} {shopData?.description}
+                            </p>
+                            <p className="text-[#353535] text-sm">
+                                <b>Email Wadusangaho ni</b> : {shopData?.email}
+                            </p>
+                            <p className="text-[#353535] text-sm">
+                                <b>Wadusang no kuri nimero ya telephone</b> : {shopData?.phone}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {showKomparasCodePanel && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                    <div className="bg-white rounded-lg shadow-lg p-4 w-80">
+                        <div className="flex justify-between items-center mb-4">
+                            <h2 className="md:text-lg font-semibold">Komparas Code:</h2>
+                            <button className="text-gray-600 hover:text-gray-900" onClick={toggleKomparasCodePanel}>&times;</button>
+                        </div>
+                        <div className="flex flex-col space-y-2">
+                            <p className="text-[#353535] text-sm">
+                                <b>Code ya Komparas</b> : {codeOfShop}
+                            </p>
                         </div>
                     </div>
                 </div>
